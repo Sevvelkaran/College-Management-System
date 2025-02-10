77b259b8d518b4da2d1335d0855d51326f72437e
LAKSITHA29 <laksitharavi@gmail.com>
2025-02-10 10:38:27 +0530

    Initial commit for class employee

---
+++ CollegeManagementSystem/src/Module/Employee.java
@@ -0,0 +1,9 @@
+package Module;
+
+public class Employee {
+    private int ID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+}
