2e9508d8a9de36d5bc7e26e07581d20582cf3e3a
LAKSITHA29 <laksitharavi@gmail.com>
2025-02-10 21:17:44 +0530

    third commit

---
+++ CollegeManagementSystem/src/Model/Student.java
@@ -0,0 +1,55 @@
+package Model;
+public class Student {
+	private int ID;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+	private String birthDate;
+	private Model.Class c;
+	
+	public Student() {}
+	
+	    public int getID{
+	    	return ID;
+	    }
+	    public void setID(int ID) {
+	    	this.ID=ID;
+	    }
+	    public String getFirstName{
+	    	return firstName;
+	    }
+	    public void setFirstName(String firstName) {
+	    	this.firstName=firstName;
+	    }
+	    public String getLastName{
+	    	return LastName;
+	    }
+	    public void setLastName(String LastName) {
+	    	this.lastName=lastName;
+	    }
+	    public int getEmail{
+	    	return email;
+	    }
+	    public void setEmail(String email) {
+	    	this.email=email;
+	    }
+	    public String getPhoneNumber{
+	    	return phoneNumber;
+	    }
+	    public void setPhoneNumber(String phoneNumber) {
+	    	this.phoneNumber=phoneNumber;
+	    }
+	    public String getBirthDate{
+	    	return birthDate;
+	    }
+	    public void setBirthDate(String birthDate) {
+	    	this.birthDate=birthDate;
+	    }
+	    public Class getCurrentClass() {
+	    	return c;
+	    }
+	    public void setClass(Class c) {
+	    	this.c=c;
+	    }
+}
