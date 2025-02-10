fdba512ee51afb5d5f2c91d7ba7bc9ba5659e915
LAKSITHA29 <laksitharavi@gmail.com>
2025-02-10 16:06:27 +0530

    second commit

--- CollegeManagementSystem/src/Module/Employee.java
+++ CollegeManagementSystem/src/Module/Employee.java
@@ -6,4 +6,41 @@ public class Employee {
     private String lastName;
     private String email;
     private String phoneNumber;
+    private double salary;
+    private Department department;
+     
+    public Employee() {}
+    
+    public int getID{
+    	return ID;
+    }
+    public void setID(int ID) {
+    	this.ID=ID;
+    }
+    public String getFirstName{
+    	return firstName;
+    }
+    public void setFirstName(String firstName) {
+    	this.firstName=firstName;
+    }
+    public String getLastName{
+    	return LastName;
+    }
+    public void setLastName(String LastName) {
+    	this.lastName=lastName;
+    }
+    public int getEmail{
+    	return email;
+    }
+    public void setEmail(String email) {
+    	this.email=email;
+    }
+    public String getPhoneNumber{
+    	return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+    	this.phoneNumber=phoneNumber;
+    }     
 }
+
+
