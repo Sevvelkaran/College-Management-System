cadd36fa9069b4a174249f6253d97567cc29a023
Sevvelkaran <2k21it50@kiot.ac.in>
2025-02-11 21:50:19 +0530

    commit

---
+++ CME_/src/Module/Class.java
@@ -0,0 +1,28 @@
+package Module;
+
+public class Class {
+
+	private int ID;
+	private String title;
+	
+	public Class() {
+	}
+	
+		public int getId() {
+			return ID;
+		}
+		
+		public void setId(int Id) {
+			this.ID = Id;
+		}
+		
+		public String getTitle() {
+			return title;
+		}
+		
+		public void setTitle(String title) {
+			this.title = title;
+		
+	}
+	
+}
---
+++ CME_/src/Module/Department.java
@@ -0,0 +1,27 @@
+package Module;
+
+public class Department {
+
+	private int ID;
+	private String title;
+	public Department() {}
+		
+		public int getId() {
+			return ID;
+		}
+		
+		public void setId(int Id) {
+			this.ID = Id;
+		}
+		
+		public String getTitle() {
+			return title;
+		}
+		
+		public void setTitle(String title) {
+			this.title = title;
+		}
+	
+	
+
+}
