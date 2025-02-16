94000028654480c61dfecdab7323912646dc724e
LAKSITHA29 <laksitharavi@gmail.com>
2025-02-16 13:25:23 +0530

    employee class updated

--- CME_/src/Model/Employee.java
+++ CME_/src/Model/Employee.java
@@ -1,5 +1,7 @@
-
 package Model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Employee {
     private int ID;
@@ -14,6 +16,29 @@ public class Employee {
      
     public Employee() {}
     
+    public Employee(int ID,Database database) {
+    	try {
+    		setID(ID);
+    		String select="SELECT 'ID'.'FirstName','LastName','Email','PhoneNumber',"
+    	     +"'BirthDate','Salary','Department','Password'"+"FROM 'employees' WHERE 'ID'="+getID()+";";
+    	
+			ResultSet rs=database.getStatement().executeQuery(select);
+			rs.next();
+			
+			setFirstName(rs.getString("FirstName"));
+			setLastName(rs.getString("LastName"));
+			setEmail(rs.getString("Email"));
+			setPhoneNumber(rs.getString("PhoneNumber"));
+			setBirthDate(rs.getString("BirthDate"));
+			setSalary(rs.getDouble("Salary"));
+			setPassword(rs.getString("Password"));
+			int deptID=rs.getInt("Department");
+			setDepartment(new Department(deptID,database));
+			
+    }catch (SQLException e) {
+    	e.printStackTrace();
+    }
+ }
     public int getID(){
     	return ID;
     }
@@ -67,5 +92,30 @@ public class Employee {
     }
     public void setPassword(String password) {
     	this.password=password;
+    }
+    public void update(Database database) {
+    	try {
+
+    		String update = "UPDATE 'employees' SET 'FirstName'='"+getFirstName()+
+    		"','LastName'='"+getLastName()+"','Email'='"+getEmail()+
+    		"','PhoneNumber'='"+getPhoneNumber()+"','BirthDate'='"+getBirthDate()+
+    		"','Salary'='"+getSalary()+"','Department'='"+getDepartment().getID()+
+    		"','Password' ='"+getPassword()+" 'WHERE 'ID'="+getID()+";";
+    		database.getStatement().execute(update); 
+    		System.out.println("Employee updated successfully");		
+
+    		} catch (SQLException e) {
+              	e.printStackTrace();
+    		}
+    }
+    public void print() {
+    	System.out.println("ID:\t\t");
+    	System.out.println("Name:\t\t"+getFirstName()+" "+getLastName());
+    	System.out.println("Email:\t\t"+getEmail());
+    	System.out.println("Phone Number:\t"+getPhoneNumber());
+    	System.out.println("BirthDate:\t"+getBirthDate());
+    	System.out.println("Salary:\t\t"+getSalary());
+    	System.out.println("Department:\t"+getDepartment());
+    	System.out.println("______________________\n");
     }
 }
